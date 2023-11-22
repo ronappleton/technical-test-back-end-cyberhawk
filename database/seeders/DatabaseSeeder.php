5bb56574ab8e166e353e4a442453c3e4344936eb
Ron Appleton <ron.appleton@gmail.com>
2023-11-22 12:21:27 +0000

    1st Run of Pint for PSR12

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,14 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Component;
-use App\Models\ComponentType;
-use App\Models\Farm;
-use App\Models\Grade;
-use App\Models\GradeType;
-use App\Models\Inspection;
-use App\Models\Turbine;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
